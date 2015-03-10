de7eb8e8f91c2d943d4c9d6db9a0c033b170b35a
Steffen Fuchs <st.fuchs.tr@gmail.com>
2015-03-10 19:08:08 +0100

    subtype map on vector

--- gdbpyext/stl.py
+++ gdbpyext/stl.py
@@ -9,3 +9,9 @@ def xVec(vec):
     "extracts gdb.Value objects from stl vector to python array"
     n =  int(vec['_M_impl']['_M_finish'] - vec['_M_impl']['_M_start'])
     return [ xVecElement(vec, i) for i in range(n) ]
+
+def xVecMap(subtype, vec):
+    v = xVec(vec)
+    for s in subtype.split("."):
+        v = list(map(lambda x: x[s], v))
+    return v
